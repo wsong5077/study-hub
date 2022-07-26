36cccce3ea15038c3da3dfe392d2f9e12a8d50bf
宋惟嘉 <feifeige@feifeideMBP.lan>
2022-07-26 12:59:17 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+/*
+import { initializeApp } from '@firebase/app';
+import { getAuth, GoogleAuthProvider } from "@firebase/auth"
+import {getFirestore} from "firebase/firestore/lite"
+import firebase from 'firebase/compat/app';
+const firebaseConfig = {
+  apiKey: "AIzaSyBmv3RM5t9qAY3lRHxRztmhqk0yLzX_cus",
+  authDomain: "study-hub-e17e2.firebaseapp.com",
+  projectId: "study-hub-e17e2",
+  storageBucket: "study-hub-e17e2.appspot.com",
+  messagingSenderId: "768170376867",
+  appId: "1:768170376867:web:37f15fbf57bc247d2ec916"
+};
+
+const app = initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getFirestore.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, getAuth, GoogleAuthProvider, provider };
+export default db;
+*/
